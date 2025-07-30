f43546dc02da54fdc027a54a25c4cdb6e34a859d
Tenet-11 <tenet113219@gmail.com>
2025-07-30 21:42:57 +0800

    Create UVA 10071.cpp

---
+++ UVA 10071.cpp
@@ -0,0 +1,13 @@
+// CPE 一星題
+// d226. 10071 - Back to High School Physics
+
+#include <iostream>
+using namespace std;
+
+// 物理: V-T圖下面積
+int main() {
+	int t, v;
+	while (cin >> v >> t) {
+		cout << 2 * v * t << endl;
+	}
+}
