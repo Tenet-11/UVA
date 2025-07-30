dd9e96d5919aee858fb650d5979e7e513e48df1a
Tenet-11 <tenet113219@gmail.com>
2025-07-30 18:53:06 +0800

    Create UVA 10055.cpp

---
+++ UVA 10055.cpp
@@ -0,0 +1,14 @@
+// CPE 一星題
+// a012. 10055 - Hashmat the Brave Warrior
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// 我還以為要不斷輸入直到EOF
+
+	long long int a, b;
+	cin >> a >> b;
+	cout << abs(a - b);
+}
