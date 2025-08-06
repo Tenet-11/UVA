32b2dfbc072895f2fa571e0aaa3b0cbf845a569e
Tenet-11 <tenet113219@gmail.com>
2025-08-06 16:55:11 +0800

    Create UVA 10420.cpp

---
+++ UVA 10420.cpp
@@ -0,0 +1,27 @@
+// CPE 一星題
+// a743. 10420 - List of Conquests
+
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+	cin.ignore();
+
+	map<string, int> country_count;
+	for (int i = 0; i < n; i++) {
+		string country;
+		cin >> country;
+		string name;
+		getline(cin, name);
+
+		country_count[country]++;
+	}
+	for (auto it : country_count) {
+		cout << it.first << " " << it.second << endl;
+	}
+}
