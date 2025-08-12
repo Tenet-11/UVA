6ce44d56a0dd68179a5b5ecc64384cec7bf556b8
Tenet-11 <tenet113219@gmail.com>
2025-08-12 15:21:04 +0800

    Update c420.cpp

--- Zerojudge 題目練習/c420.cpp
+++ Zerojudge 題目練習/c420.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	
-	for (int i = 1; i <= n + 2; i+=2) {
+
+	int width = 2 * n - 1;
+	for (int i = 1; i <= width; i += 2) {
 		// 這寫法聰明，學一下
-		
+
 		// string(len, char) 是 C++ 的一個建構子，可以
 		// 建立一個長度為 len、每個位置都是 char 的字串。
-		cout << string((n + 2 - i) / 2, '_');
-		cout << string(i, '*');
-		cout << string((n + 2 - i) / 2, '_') << endl;
+		int pad = (width - i) / 2;
+		cout << string(pad, '_') << string(i, '*') << string(pad, '_') << endl;
+
 	}
 }
