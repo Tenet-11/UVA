4118e7e1697d4a53a9dd6d0a31918a6fe36284a3
Tenet-11 <tenet113219@gmail.com>
2025-08-20 14:52:37 +0800

    Create i025.cpp

---
+++ Zerojudge 題目練習/i025.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	long long int n;
+	cin >> n;
+	
+	long long int count = 0;
+	for (long long int i = 1; i < n; i++) {
+		if (n % i == 0)count += i;
+	}
+	cout << count;
+}
