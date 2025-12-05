f5c07db1efcf5a6400f4f53ebe931f82e99792e2
Tenet-11 <tenet113219@gmail.com>
2025-12-05 23:59:39 +0800

    Update UVA 10093.cpp

--- UVA 10093.cpp
+++ UVA 10093.cpp
@@ -1,48 +1,40 @@
 // CPE 一星題
 
 #include <iostream>
-
 using namespace std;
-
-// 這題滿難的
-int getvalue(char c) {
-	if (isdigit(c)) return c - '0';
-	if (isupper(c)) return c - 'A' + 10;
-	if (islower(c)) return c - 'a' + 36;
-	return -1;
-}
-
+ 
 int main() {
-	string s;
-	while (cin >> s) {
-		int max_digit = 0;
-		int start = 0;
-		if (s[0] == '+' || s[0] == '-') {
-			start = 1;
-		}
-		for (int i = start; i < s.length(); i++)
-		{
-			int value = getvalue(s[i]);
-			if (value > max_digit) max_digit = value;
-		}
-
-
-		bool found = false;
-		for (int base = max_digit + 1; base <= 62; base++) {
-			long long int  Value = 0; // 放值
-			for (int j = start; j < s.length(); j++) {
-				Value = Value * base + getvalue(s[j]);
-			}
-
-			if (Value % (base - 1) == 0) {
-				cout << base << endl;
-				found = true;
-				break;
-			}
-		}
-
-		if (!found) {
-			cout << "such number is impossible!" << endl;
-		}
-	}
+    int sum, mx, temp;
+    string s;
+    while (getline(cin, s)){
+        sum = 0;
+        mx = 1;
+        int i;
+        for (i = 0; i < s.size(); i++) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                temp = s[i] - '0';
+            }
+            else if (s[i] >= 'A' && s[i] <= 'Z') {
+                temp = s[i] - 'A' + 10;
+            }
+            else if (s[i] >= 'a' && s[i] <= 'z') {
+                temp = s[i] - 'a' + 36;
+            }
+            else continue;
+ 
+            if (mx < temp)
+                mx = temp;
+            sum += temp;
+        }
+ 
+        for (i = mx; i < 62; i++)
+            if (!(sum % i)) {
+                cout << i + 1 << "\n";
+                break;
+            }
+ 
+        if (i == 62)
+            cout << "such number is impossible!\n";
+    }
+    return 0;
 }
